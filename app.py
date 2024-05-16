5368effe4f5fbdc211d32f7731d1b41b340057fe
Anderson Smith <acs374@cornell.edu>
2024-05-16 14:01:34 -0500

    Implemented flashcard-app  concept in an object oriented manner and created very basic GUI

---
+++ app.py
@@ -0,0 +1,94 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+
+
+class FlashcardApp():
+    """
+    A flashcard application that helps the user
+    memorize terms and definitions via spaced repition
+
+    Attributes
+        root: a tkinter root
+    """
+    def __init__(self, root):
+        self.root = root
+
+        # creating main window 
+        self.root.title("Flashcard App")
+        self.root.geometry('1000x700')
+
+        # create variables
+        self.create_variables()
+
+        # create notebook/tabs + buttons
+        self.generate_notebook() # self initializes notebook
+        self.set_tab()
+        self.study_tab()
+
+
+        # style/color
+        #TODO
+
+    def create_variables(self):
+        self.set_name = tk.StringVar()
+        self.term = tk.StringVar()
+        self.definition = tk.StringVar()
+        self.card_index = 0
+        self.current_tabs = []
+
+
+    def generate_notebook(self):
+        self.notebook = ttk.Notebook(self.root)
+        self.notebook.pack(fill='both', expand=True)
+        
+        # sets frame
+        self.generate_set_frame = ttk.Frame(self.notebook)
+        self.notebook.add(self.generate_set_frame, text='Create Set')
+
+        # entry widgets
+        ttk.Label(self.generate_set_frame, text='Set Name: ').pack(padx=5, pady=5)
+        ttk.Label(self.generate_set_frame, textvariable=self.set_name, width=30).pack(padx=5, pady=5)
+
+        ttk.Label(self.generate_set_frame, text='Term: ').pack(padx=5, pady=5)
+        ttk.Label(self.generate_set_frame, textvariable=self.term, width=30).pack(padx=5, pady=5)
+
+        ttk.Label(self.generate_set_frame, text='Definition: ').pack(padx=5, pady=5)
+        ttk.Label(self.generate_set_frame, textvariable=self.definition, width=30).pack(padx=5, pady=5)
+
+        # add word button (add_word)
+        self.add_word_button = ttk.Button(self.generate_set_frame, text='Add Word').pack(padx=5, pady=10)
+
+        # save set button (create_set)
+        self.save_set_button = ttk.Button(self.generate_set_frame, text='Save Set').pack(padx=5, pady=10)
+    
+    def set_tab(self):
+        self.select_set_frame = ttk.Frame(self.notebook)
+        self.notebook.add(self.select_set_frame, text='Select Set')
+
+        existing_sets = ttk.Combobox(self.select_set_frame, state='readonly')
+
+        # select set button (select_set)
+        self.select_set_button = ttk.Button(self.select_set_frame, text='Select Set').pack(padx=5, pady=5)
+
+        # delete set button
+        self.delete_set_button = ttk.Button(self.select_set_frame, text='Delete Set').pack(padx=5, pady=5)
+
+    def study_tab(self):
+        self.flashcards_frame = ttk.Frame(self.notebook)
+        self.notebook.add(self.flashcards_frame, text='Study' )
+
+        # display term  and definition on flashcard
+        self.term_label = ttk.Label(self.flashcards_frame, text='', font=('TkDefaultFont', 24))
+        self.term_label.pack(padx=5,pady=10)
+
+        self.definition_label = ttk.Label(self.flashcards_frame, text='').pack(padx=5, pady=5)
+
+        # flip flashcards (flip_card)
+        self.flip_card_button = ttk.Button(self.flashcards_frame, text='Flip').pack(side='left',padx=5,pady=5)
+
+        # button to view next card (next_card)
+        self.next_card_button = ttk.Button(self.flashcards_frame, text='Next').pack(side='right', padx=5,pady=5)
+
+        # button to view previous card previous_card
+        self.previous_card_button = ttk.Button(self.flashcards_frame, text='Previous Card').pack(side='left',padx=5,pady=5)
--- main.py
+++ main.py
@@ -1,52 +1,12 @@
-import os
-
-def clear_terminal():
-    # For Windows
-    if os.name == 'nt':
-        os.system('cls')
-    # For Unix-based systems (Linux, macOS)
-    else:
-        os.system('clear')
-
-class FlashCard():
-    """
-    A flashcard that has attributes:
-    term: a string
-    definition: a string
-    """
-    def __init__(self, term, definition):
-        self.term = str(term)
-        self.definition = str(definition)
+import tkinter as tk
+from tkinter import ttk
+from app import FlashcardApp
+"""
+This program allows you to run the FlashcardApp
+created in file 'app.py'
+"""
 
 if __name__ == '__main__':
-    while True:
-        flashcards = []
-        while True:
-            clear_terminal()
-            flashcard_term = input('Please enter the term of your flashcard. Enter nothing to study: ')
-            if flashcard_term == '':
-                clear_terminal()
-                break
-            flashcard_definition = input('Please enter the definition of your flashcard: ')
-            clear_terminal()
-            flashcard = FlashCard(flashcard_term, flashcard_definition)
-            flashcards.append(flashcard)
-
-        attempts = 0
-        while len(flashcards) > 0:
-            for i, card in enumerate(flashcards):
-                answer = input(f'What is the definition for the term {card.term}? ')
-                if answer == card.definition:
-                    clear_terminal()
-                    print('Thats the correct answer!')
-                    flashcards.pop(i)
-                else:
-                    clear_terminal()
-                    print('Wrong, try again after studying the rest of the cards!')
-            attempts += 1 
-        
-        print('You have finished studying!')
-        print(f'It took you {attempts} attempts!')
-        break
-            
-
+    root = tk.Tk()
+    app = FlashcardApp(root)
+    root.mainloop()
